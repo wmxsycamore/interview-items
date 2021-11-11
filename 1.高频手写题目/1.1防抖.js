1427faf3905e71cb0577422fbff97c7db9f303ee
wmxsycamore <2686513404@qq.com>
2021-11-11 22:35:27 +0800

    feat:分类

--- 1.高频手写题目/1.1防抖.js
+++ 1.高频手写题目/1.1防抖.js
@@ -3,7 +3,7 @@ const debounce = function(func, wait = 50) {
   let timer = 0;
   return function (...params) {
     if(timer) clearInterval(timer);
-    setTimeout(() => {
+    timer = setTimeout(() => {
       func.apply(this, params);
     }, wait);
   }
